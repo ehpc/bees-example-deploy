3bc7dd9823c366e3a35e52c4f67bc7297d73f027
ehpc <ehpc@ehpc.io>
2020-06-25 10:07:00 +0300

    Add db connection

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Bee = mongoose.model('Bee', { test: Number });
 
 const app = express();
 
-app.get('/', (req, res) => res.send('HELLO!'));
+app.get('/', async (req, res) => {
+  await new Bee({
+    test: 453545
+  }).save();
+  const bees = await Bee.find();
+  res.send(`HELLO! ${JSON.stringify(bees)}`);
+});
 
-app.listen(process.env.PORT ?? 3000);
+app.listen(process.env.PORT);
